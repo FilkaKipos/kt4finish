aa645cabdb512be1385ab156163569fb1c1b9dcb
FilkaKipos <rezidreta@gmail.com>
2023-10-28 11:29:14 +0500

    final

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,42 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData (scaffoldBackgroundColor: Color(0xFF262B30),
+      textTheme: TextTheme(
+          bodyMedium: TextStyle(color:Color.fromARGB(255, 255, 255, 255)),
+        ),),
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Color(0xFF262B30),
           title: Row(
             children: [
               IconButton(
+                style: ButtonStyle(
+              textStyle: MaterialStateProperty.all<TextStyle>(
+                TextStyle(color: const Color.fromARGB(255, 255, 255, 255)))),
                 icon: Icon(Icons.close_outlined),
                 onPressed: () {
                 },
+                
               ),
               SizedBox(width: 8),
-              Text('Войти'),
+              Text('Войти')
+              ,
             ],
           ),
         ),
         body: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(40.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              SizedBox(height: kToolbarHeight), // Добавлено для отступа под AppBar
               TextField(
                 decoration: InputDecoration(
                   labelText: 'Имя пользователя, почту или номер телефона',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
                 ),
               ),
               SizedBox(height: 16.0),
@@ -40,35 +52,49 @@ class LoginPage extends StatelessWidget {
                 obscureText: true,
                 decoration: InputDecoration(
                   labelText: 'Пароль',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
                 ),
               ),
               SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  primary: Color.fromARGB(255, 55, 107, 57),
+                  backgroundColor: Color.fromARGB(255, 55, 107, 57),
+                  
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13.0),
+                  ),
                 ),
                 child: Text('Войти'),
               ),
-              SizedBox(height: 16.0),
+              SizedBox(height: 16.0,),
               ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  primary: Color.fromARGB(1, 34, 39, 43),
+                  backgroundColor: Color.fromARGB(255, 45, 50, 54),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13.0),
+                  ),
                 ),
                 child: Text('Создать Аккаунт'),
               ),
               SizedBox(height: 8),
-              Text('или', style: TextStyle(fontSize: 16)),
+              Text('или',textAlign: TextAlign.center , style: TextStyle(fontSize: 16)),
               SizedBox(height: 16.0,),
               OutlinedButton.icon(
                 onPressed: () {
                 },
                 style: OutlinedButton.styleFrom(
-                  primary: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13.0),
+                  ),
+                  backgroundColor: Colors.red,
                   side: BorderSide(color: Colors.red),
                 ),
-                icon: Icon(Icons.login, color: Colors.red),
+                
+                icon: Icon(Icons.login, color: const Color.fromARGB(255, 255, 255, 255)),
                 label: Text('Войти с помощью Google'),
               ),
               SizedBox(height: 16.0),
@@ -77,11 +103,16 @@ class LoginPage extends StatelessWidget {
                   
                 },
                 style: OutlinedButton.styleFrom(
-                  primary: Color(0xFF2D42A0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13.0),
+                  ),
+                  backgroundColor: Color(0xFF2D42A0),
                   side: BorderSide(color: Color(0xFF2D42A0)),
                 ),
-                icon: Icon(Icons.login, color: Color(0xFF2D42A0)),
-                label: Text('Войти с помощью BK'),
+                icon: Icon(Icons.login, color: Color.fromARGB(255, 255, 255, 255)),
+                label: Text('Войти с помощью BK')
+                
+                ,
               ),
             ],
           ),
